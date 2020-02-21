c5bef365453acc2c3209c2b7a54d170d48cf622e
anilCSE <anil@vitwit.com>
2020-02-21 22:28:27 +0530

    Add debug logs

--- util/uptime/src/uptime.go
+++ util/uptime/src/uptime.go
@@ -190,6 +190,7 @@ func CalculateUpgradePoints(startBlock int64, valUpgradeBlock int64, endBlockHei
 	var rewards int64 = 0
 
 	if valUpgradeBlock == 0 {
+		fmt.Println("valUpgradeBlock-----": valUpgradeBlock, missedDeductionFactor)
 		return rewards
 	}
 
